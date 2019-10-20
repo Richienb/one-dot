f7b72dde4fac38699da865d25e86699951211103
Richie Bendall <richiebendall@gmail.com>
2019-10-21 00:37:38 +1300

    Proper handling of provided domain name

--- test.js
+++ test.js
@@ -11,27 +11,27 @@ test("main", async (t) => {
         t.is(typeof value.data, "string")
     })
 
-    await t.throwsAsync(() => oneDot({ name: "google.com", type: "a", method: "someInvalidMethod" }), {
+    await t.throwsAsync(() => oneDot({ domain: "google.com", type: "a", method: "someInvalidMethod" }), {
         instanceOf: ReferenceError,
         message: "Invalid method specified!",
     })
 
-    await t.throwsAsync(() => oneDot({ name: "google.com" }), {
+    await t.throwsAsync(() => oneDot({ domain: "google.com" }), {
         instanceOf: ReferenceError,
         message: "Type not specified!",
     })
 
     await t.throwsAsync(() => oneDot({ type: "a" }), {
         instanceOf: ReferenceError,
-        message: "Name not specified!",
+        message: "Domain not specified!",
     })
 
-    verifyData(await oneDot({ name: "google.com", type: "a", method: "https" }))
-    verifyData(await oneDot({ name: "google.com", type: "a", method: "tls" }))
-
-    await t.throwsAsync(() => oneDot({ name: "google.com", type: "thisIsInvalid" }), {
+    await t.throwsAsync(() => oneDot({ domain: "google.com", type: "thisIsInvalid" }), {
         instanceOf: DNSError,
         message: "DNS query not specified or too small.",
         code: 400,
     })
+
+    verifyData(await oneDot({ domain: "google.com", type: "a", method: "https" }))
+    verifyData(await oneDot({ domain: "google.com", type: "a", method: "tls" }))
 })
